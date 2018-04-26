d7bcbfe62000bdf2d42a18139fb7b63539eb82a5
SlaviTod <slavka_todorova@abv.bg>
2018-04-26 18:59:51 +0300

    delete some unused staff

--- server/data_layer/data_layer.js
+++
@@ -1,9 +0,0 @@
-const data = require('../data');
-
-module.exports = {
-    UsersData: data.user,
-    RolesData: data.role,
-    PrivilegesData: data.privilege,
-    TicketsData: data.ticket,
-    TeamsData: data.team,
-}
