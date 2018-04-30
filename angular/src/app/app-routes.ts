a14527a45707d528f1140a914d6c9f67992eb7ed
SlaviTod <slavka_todorova@abv.bg>
2018-04-30 10:35:22 +0300

    add router, create login component, install @angular/materials

---
+++ angular/src/app/app-routes.ts
@@ -0,0 +1,8 @@
+import { LoginComponent } from './login/login.component'
+import { Routes } from '@angular/router';
+
+export const ROUTES: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full'}, 
+  // { path: '**', component: NotFoundComponent} // TODO add component 
+];
---
+++ angular/src/app/ticket/ticket.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TicketRoutingModule } from './ticket-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    TicketRoutingModule
+  ],
+  declarations: []
+})
+export class TicketModule { }
