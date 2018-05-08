fd516912337e8ebe329f5d65123adbaf3c702cf4
SlaviTod <slavka_todorova@abv.bg>
2018-05-08 04:21:26 +0300

    sthg that works

--- angular/src/app/ticket/ticket-routes.ts
+++ angular/src/app/ticket/ticket-routes.ts
@@ -12,25 +12,77 @@ import { GetAllTicketsOfTeamResolver } from '../core/resolvers/tickets/get-all-t
 import { AllTeamsComponent } from '../teams/all-teams/all-teams.component';
 import { GetAllAssignToTicketsOfLoggedUserInTeamResolver } from '../core/resolvers/tickets/get-all-assign-to-logged-user-in-team-resolver';
 import { GetAllTicketsRequestedByLoggedUserResolver } from '../core/resolvers/tickets/get-all-logged-user-requester-resolver';
+import { TicketCreateComponent } from './ticket-create/ticket-create.component';
+import { GetAllUsersResolver } from '../core/resolvers/get-all-users.resolver';
+import { GetAllTeamsResolver } from '../core/resolvers/get-all-teams.resolver';
+import { GetAllStatusResolver } from '../core/resolvers/get-all-statuses-resolver';
+import { GetAllImportanceResolver } from '../core/resolvers/get-all-importances-resolver';
 
 
 
 export const ROUTES: Routes = [
     {
         path: '', component: TicketPanelComponent, canActivate: [IsLogged],
+        resolve: {
+        },
         children: [
-            { path: 'all', component: TicketListComponent, resolve: { tickets: GetAllAssignToLoggedUserResolver } },
-            { path: 'assignto', component: TicketListComponent, resolve: { tickets: GetAllAssignToLoggedUserResolver } },
+            {
+                path: 'all', component: TicketListComponent, resolve: {
+                    tickets: GetAllAssignToLoggedUserResolver, statuses: GetAllStatusResolver,
+                    importances: GetAllImportanceResolver,
+                }
+            },
+            {
+                path: 'assignto', component: TicketListComponent, resolve: {
+                    tickets: GetAllAssignToLoggedUserResolver,
+                    statuses: GetAllStatusResolver,
+                    importances: GetAllImportanceResolver,
+                }
+            },
             { path: 'assignto/detail/:ticketId', component: TicketDetailComponent, resolve: { ticket: TicketDetailsByIdResolver } },
-            { path: 'assigntointeam', component: AllTeamsComponent, resolve: { teams:  GetTeamsByLoggedUserResolver } },
-            { path: 'assigntointeam/:teamId', component: TicketListComponent, resolve: { tickets: GetAllAssignToTicketsOfLoggedUserInTeamResolver } },
-            { path: 'assigntointeam/:teamId/details/:ticketId', component: TicketDetailComponent, resolve: { ticket: TicketDetailsByIdResolver } },
+            { path: 'assigntointeam', component: AllTeamsComponent, resolve: { teams: GetTeamsByLoggedUserResolver } },
+            {
+                path: 'assigntointeam/:teamId', component: TicketListComponent, resolve: {
+                    tickets: GetAllAssignToTicketsOfLoggedUserInTeamResolver, statuses: GetAllStatusResolver,
+                    importances: GetAllImportanceResolver,
+                }
+            },
+            {
+                path: 'assigntointeam/:teamId/details/:ticketId', component: TicketDetailComponent, resolve: {
+                    ticket: TicketDetailsByIdResolver, statuses: GetAllStatusResolver,
+                    importances: GetAllImportanceResolver,
+                }
+            },
             { path: 'allofteam', component: AllTeamsComponent, resolve: { teams: GetTeamsByLoggedUserResolver } },
-            { path: 'allofteam/:teamId', component: TicketListComponent, resolve: { tickets: GetAllTicketsOfTeamResolver } },
-            { path: 'allofteam/:teamId/details/:ticketId', component: TicketDetailComponent, resolve: { ticket: TicketDetailsByIdResolver } },
+            {
+                path: 'allofteam/:teamId', component: TicketListComponent, resolve: {
+                    tickets: GetAllTicketsOfTeamResolver, statuses: GetAllStatusResolver,
+                    importances: GetAllImportanceResolver,
+                }
+            },
+            {
+                path: 'allofteam/:teamId/details/:ticketId', component: TicketDetailComponent, resolve: {
+                    ticket: TicketDetailsByIdResolver, statuses: GetAllStatusResolver,
+                    importances: GetAllImportanceResolver,
+                }
+            },
             { path: 'requested', component: TicketListComponent, resolve: { tickets: GetAllTicketsRequestedByLoggedUserResolver } },
-            { path: 'requested/details/:ticketId', component: TicketDetailComponent, resolve: { ticket: TicketDetailsByIdResolver } },
-
+            {
+                path: 'requested/details/:ticketId', component: TicketDetailComponent, resolve: {
+                    ticket: TicketDetailsByIdResolver, statuses: GetAllStatusResolver,
+                    importances: GetAllImportanceResolver,
+                }
+            },
+            {
+                path: 'create', component: TicketCreateComponent,
+                resolve: {
+                    statuses: GetAllStatusResolver,
+                    importances: GetAllImportanceResolver,
+                    // users: GetAllUsersResolver, 
+                    //         teams: GetAllTeamsResolver,
+                    // ticket: TicketDetailsByIdResolver 
+                }
+            },
             { path: '', redirectTo: 'assignto' },
 
         ],
